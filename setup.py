feb33f23a4bb32767daead06c89764cf8a9fd3c3
Alex Hall <alex.mojaki@gmail.com>
2019-07-03 14:43:53 +0200

    No 3.8 support for now

--- setup.py
+++ setup.py
@@ -21,6 +21,5 @@ setup(name=package,
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
       ],
       zip_safe=False)
