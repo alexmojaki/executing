90c000973b6cdfa4f4f575f7e2cadfd18c881412
Alex Hall <alex.mojaki@gmail.com>
2022-02-27 23:14:27 +0200

    test_ipython_cell_code

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from types import SimpleNamespace
+
+from executing.executing import is_ipython_cell_code
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
@@ -17,3 +20,26 @@ def test_pytest():
     ), 'message'
     x = tester.x
     assert x is tester
+
+
+def test_ipython_cell_code():
+    assert is_ipython_cell_code(
+        SimpleNamespace(
+            co_name="<cell line: 1>",
+            co_filename="tmp/ipykernel_3/foo",
+        )
+    )
+
+    assert not is_ipython_cell_code(
+        SimpleNamespace(
+            co_name="<cell line: 1",
+            co_filename="tmp/ipykernel_3/foo",
+        )
+    )
+
+    assert not is_ipython_cell_code(
+        SimpleNamespace(
+            co_name="<cell line: 1>",
+            co_filename="tmp/ipykernel_3",
+        )
+    )
