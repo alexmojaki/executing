c1709e479f08d3bbd1f259bcc1c4cde9f173df3b
Frank Hoffmann <15r10nk-git@polarbit.de>
2022-09-18 14:24:03 +0200

    test: test tester with `test_tester`

--- tests/test_main.py
+++ tests/test_main.py
@@ -1376,23 +1376,9 @@ def lambda_maker():
 lamb = lambda: 0
 
 
-assert tester([1, 2, 3]) == [1, 2, 3]
-
-assert tester.asd is tester
-assert tester[1 + 2] is tester
-assert tester ** 4 is tester
-assert tester * 3 is tester
-assert tester - 2 is tester
-assert tester + 1 is tester
-assert -tester is +tester is ~tester is tester
-assert (tester < 7) is tester
-assert (tester >= 78) is tester
-assert (tester != 79) is tester
-# assert (5 != tester != 6) is tester
-assert tester.foo(45, False) == 45
-
-assert (tester or 234) == 234
-assert (tester and 1123) is tester
+def test_tester():
+    # tester calls should be tested at global scope
+    from . import global_tester_calls
 
 
 def empty_decorator(func):
